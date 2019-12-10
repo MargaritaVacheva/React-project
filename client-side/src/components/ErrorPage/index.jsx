b61426e920bd142430034e4910d3e0e67a24535e
MargaritaVacheva <m.d.vacheva@gmail.com>
2019-12-11 01:19:08 +0200

    error page img

--- client-side/src/components/ErrorPage/index.jsx
+++ client-side/src/components/ErrorPage/index.jsx
@@ -4,8 +4,10 @@ import background from '../../photos/angelina-jollivet-SeCt2NnTecM-unsplash.jpg'
 
 const ErrorPage = () => {
     return (
-        <section className="error-section" style={{backgroundImage: background}}>
-            <h1>Oh no! Someone ate all cookies! We`re sorry..</h1>
+        <section className="error-section" style={{backgroundImage: `url(${background})`}}>
+            {/* <img src={background} alt="error" /> */}
+            <h1>Oh no! Someone ate all cookies!</h1>
+            <h2> 404. We`re sorry...</h2>
             <Link to="/">Go to home page for more :)</Link>
         </section>);
 }
