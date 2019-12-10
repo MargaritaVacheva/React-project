464d4b2c7f531eef6da4a5c4719595ba4d52b18c
MargaritaVacheva <m.d.vacheva@gmail.com>
2019-12-11 00:35:52 +0200

    folder structure changed

--- client-side/src/components/ErrorPage/index.jsx
+++ client-side/src/components/ErrorPage/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import background from '../../photos/angelina-jollivet-SeCt2NnTecM-unsplash.jpg';
 
 const ErrorPage = () => {
     return (
-        <section className="error-section">
+        <section className="error-section" style={{backgroundImage: background}}>
             <h1>Oh no! Someone ate all cookies! We`re sorry..</h1>
             <Link to="/">Go to home page for more :)</Link>
         </section>);
